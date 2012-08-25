a196eebe9e0a038e202e52b9e43b23620a9341a4
Ceaser Larry <clarry@divergentlogic.com>
2012-08-25 10:49:20 -0600

    Fixing the path to the ruby executable and cleaning up

--- definitions/debian-lenny-amd64/postinstall.sh
+++ definitions/debian-lenny-amd64/postinstall.sh
@@ -12,12 +12,13 @@ sed -i -e 's/vagrant ALL=(ALL) ALL/vagrant ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 #Installing ruby
 apt-get -y install ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb make g++ libshadow-ruby1.8
+apt-get clean
 
 # Install RubyGems 1.8.24
 wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
 tar xzf rubygems-1.8.24.tgz
 pushd rubygems-1.8.24
-/usr/bin/ruby/bin/ruby setup.rb
+/usr/bin/ruby setup.rb
 popd
 rm -rf rubygems-1.8.24*
 ln -sfv /usr/bin/gem1.8 /usr/bin/gem
